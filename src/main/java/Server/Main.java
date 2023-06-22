0d74bca194751846fd6daf1a3fd77e135922ac77
CyanideByte <cyanidebyte@hotmail.com>
2023-06-22 15:11:20 -0700

    uploading fixed

--- src/main/java/Server/Main.java
+++ src/main/java/Server/Main.java
@@ -3,6 +3,8 @@ package Server;
 
 import static spark.Spark.*;
 
+import javax.servlet.MultipartConfigElement;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -23,10 +25,12 @@ public class Main {
         // Register your endpoints here
         //get("/hello", (req, res) -> "Hello, world!");
            // Enable CORS for all routes
-        before((request, response) -> {
+    	before((request, response) -> {
             response.header("Access-Control-Allow-Origin", "*");
-            response.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-            response.header("Access-Control-Allow-Headers", "Content-Type");
+            response.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            response.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+            response.header("Access-Control-Allow-Credentials", "true");
+            request.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("/tmp"));
         });
         // Example endpoint using SynthesizeSpeechWithChosenVoiceEndpoint class
         SynthesizeSpeechWithChosenVoiceEndpoint synthesizeSpeechEndpoint = new SynthesizeSpeechWithChosenVoiceEndpoint();
